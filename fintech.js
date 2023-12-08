6c67cd22d30892375ad863dec0cea51330b164e8
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-08 10:56:34 +0100

    updated

--- fintech.js
+++ fintech.js
@@ -2,17 +2,36 @@ document.addEventListener("DOMContentLoaded",()=>{
     const proceed = document.getElementById("proceed"); 
     const form = document.getElementById("transferform")
  
-form.addEventListener("submit",(event) =>{
+
+    form.addEventListener("submit",(event) =>{
     event.preventDefault();
     withdrawal();
 });
+
+
 proceed.addEventListener("click", ()=>{
     withdrawal();
 });
 
+
+ //Toggle available balance on the index page
+ const balance = document.getElementById("balance");
+ const show = document.getElementById("show");
+
+ // balance.value = Bal i want to make the balance gotten from the form to show on the home page show balance field
+ show.addEventListener("click",()=>{
+     if(balance.style.display =="none"){
+          balance.style.display= "block";
+     }
+      else{
+          balance.style.display="none"
+      }
+     
+ })
+
 })
 
-//withdrawal
+//withdrawal on transferform page
 function withdrawal(){ 
     const enteredAmount = document.getElementById("enteredAmount").value.trim();
     let avalableBal = document.getElementById("avalableBal")
@@ -21,23 +40,31 @@ function withdrawal(){
     if(!isNaN(enteredAmount) && enteredAmount <= Bal){
        Bal -= enteredAmount
        avalableBal.value = Bal
+    //    enteredAmount= "" i want to clear the entered amount value from showing after the form submits
         alert("Transactions successful")
     }else{
         alert("insufficient funds")
     }
     }
 
-    //Toggle available balance
-    const balance = document.getElementById("balance");
-    const show = document.getElementById("show");
+   
 
-    // balance.value = Bal
-    show.addEventListener("click",()=>{
-        if(balance.style.display =="none"){
-             balance.style.display= "block";
-        }
-         else{
-             balance.style.display="none"
-         }
-        
-    })
+    //trim username on login page
+    const username = document.getElementById("user").value.trim();
+
+    //password validation on login page
+const password =document.getElementById("userpass");
+const checkbox = document.getElementById("hide_show")
+// function password(){
+
+// }
+
+// toggle password on login page
+checkbox.addEventListener("click",()=>{
+    if(password.type == "password"){
+        password.type ="text"
+    }else{
+        password.type="password"
+    }
+})
+
